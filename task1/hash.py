8f85c4bdd3e1b72ebe3b390cc06a15fb2c4e0132
Devon Martin <devmart10@gmail.com>
2018-10-16 19:29:11 -0700

    calculates Hamming distance of two input strings

--- task1/hash.py
+++ task1/hash.py
@@ -3,16 +3,32 @@
 import hashlib
 
 def main():
-    # loop until user terminates program
-    while True:
-        input_string = input()
-        encrypted = encrypt(input_string)
-        print(encrypted)
+    input1 = input('String 1:')
+    input2 = input('String 2:')
+    encrypted1 = encrypt(input1)
+    encrypted2 = encrypt(input2)
+    print(encrypted1)
+    print(encrypted2)
+    diff = calc_hamming(encrypted1, encrypted2)
+    print('Different bits:', diff)
 
 # expects a string
 def encrypt(data_in):
     encrypted = hashlib.sha256(data_in.encode()).hexdigest()
     return encrypted
+
+def calc_hamming(a, b):
+    # binary is complicated in python
+    # simply get the binary digits of the inputs
+    num_bits = max(len(a), len(b)) * 4
+    a_bin = bin(int(a, 16))[2:].zfill(num_bits)
+    b_bin = bin(int(b, 16))[2:].zfill(num_bits)
+
+    count = 0
+    for i in range(num_bits):
+        if a_bin[i] != b_bin[i]:
+            count += 1
+    return count
 
 if __name__ == '__main__':
     main()
