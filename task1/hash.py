74e83329b5dff75ac7d3c46ae9935ef94d9c7356
Devon Martin <devmart10@gmail.com>
2018-10-16 19:35:38 -0700

    touch ups

--- task1/hash.py
+++ task1/hash.py
@@ -1,14 +1,20 @@
 # hash.py
-
 import hashlib
 
 def main():
+    # user input strings
     input1 = input('String 1:')
     input2 = input('String 2:')
+
+    # encrypt them both using sha256
     encrypted1 = encrypt(input1)
     encrypted2 = encrypt(input2)
+
+    # display hashed data
     print(encrypted1)
     print(encrypted2)
+
+    # calc and show bit difference
     diff = calc_hamming(encrypted1, encrypted2)
     print('Different bits:', diff)
 
@@ -17,6 +23,8 @@ def encrypt(data_in):
     encrypted = hashlib.sha256(data_in.encode()).hexdigest()
     return encrypted
 
+# calculate the Hamming distance of two inputs
+# expects binary encoded data
 def calc_hamming(a, b):
     # binary is complicated in python
     # simply get the binary digits of the inputs
@@ -24,6 +32,7 @@ def calc_hamming(a, b):
     a_bin = bin(int(a, 16))[2:].zfill(num_bits)
     b_bin = bin(int(b, 16))[2:].zfill(num_bits)
 
+    # count differing bits
     count = 0
     for i in range(num_bits):
         if a_bin[i] != b_bin[i]:
