fbbae4433027db5416f6736b29b170ef560dba7e
Devon Martin <devmart10@gmail.com>
2018-10-26 09:55:59 -0700

    rsa is working!

--- task4/rsa.py
+++ task4/rsa.py
@@ -1,26 +1,39 @@
 import binascii
+from Crypto.PublicKey import RSA
 
 def main():
-	# select two primes
-	P = 53
-	Q = 59
+	# # select two primes
+	# P = 53
+	# Q = 59
 
-	# calc public key
+	# # calc public key
+	# n = P * Q
+	# print('n:', n)
+
+	# # select an exponenent
+	# e = 3
+	# print('e:', e)
+
+	# # calc phi(n)
+	# phi_n = (P - 1) * (Q - 1)
+	# print('phi_n:', phi_n)
+
+	# # calc private key
+	# k = 2
+	# d = (k * phi_n + 1) // e
+	# print('d:', d)
+
+	e = 65537
+	bits = 2048
+	key = RSA.generate(bits, e=e)
+	P = key.p
+	Q = key.q
 	n = P * Q
-	print('n:', n)
+	# d = key.d
 
-	# select an exponenent
-	e = 3
-	print('e:', e)
-
-	# calc phi(n)
 	phi_n = (P - 1) * (Q - 1)
-	print('phi_n:', phi_n)
-
-	# calc private key
-	k = 2
-	d = (k * phi_n + 1) // e
-	print('d:', d)
+	inv = modinv(e, phi_n)
+	d = pow(inv, 1, phi_n)
 
 	# encrypt message
 	m = 'hello world'
@@ -43,6 +56,22 @@ def gcd(a, b):
 # TODO
 def multiplicative_inverse(e, phi):
 	pass
+
+# """
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+# """
 
 if __name__ == '__main__':
 	main()
